3809edb46ad8e3f450f966fd07ea41fc71daf1a8
Nicholas Mendez <mendez.nicholasr@gmail.com>
2022-01-17 12:26:37 -0500

    completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName(){
+
+    customerName = customerName.toUpperCase();
+    return customerName;
+}
+
+function setBestCustomer() {
+   bestCustomer = 'not bob' ;
+   return bestCustomer;
+}
+
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+    
+}
+
+const leastFavoriteCustomer = 'jane';
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'moby';
+}
